d22c8abf9a9434bb33945a6e1c77e44d41d02944
iinteger <42987286+iinteger@users.noreply.github.com>
2021-06-18 12:19:24 +0900

    add comment

--- canny.py
+++ canny.py
@@ -1,4 +1,4 @@
-# 이미지체서 엣지를 추출해 저장
+# 이미지체서 엣지를 추출해 저장, load가 안되는 이미지는 삭제
 # canny threshold는 heuristic하게 결정함
 
 import os
@@ -10,12 +10,12 @@ dataset = "twilight landscape"  # data folder
 
 color_path = "images/"+dataset+"/"
 image_ids = glob.glob(color_path+"*")
-path = "images/"+dataset+" edge/"
+edge_path = "images/"+dataset+" edge/"
 
 for i in image_ids:
-    try:  # 간혹 load가 안되는 경우가 있음
+    try:  # load가 안되는 이미지는 예외처리로 삭제
         canny = ~cv2.Canny(cv2.imread(i), random.randrange(100, 201), random.randrange(500, 601))
     except:
         os.remove(i)
         continue
-    cv2.imwrite(path+i[len(color_path):], canny)
+    cv2.imwrite(edge_path+i[len(color_path):], canny)
